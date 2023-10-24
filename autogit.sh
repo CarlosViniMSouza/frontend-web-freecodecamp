8b17a8bb53ab6b0be9eb221360fe6dd988f3068e
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-10-23 22:31:31 -0300

    stopped: JS Variables

--- autogit.sh
+++ autogit.sh
@@ -1,9 +1,6 @@
-git init
 git add .
-git commit -m "project commited!"
-git branch -M main
-git remote add origin https://github.com/CarlosViniMSouza/frontend-web-freecodecamp.git
+git commit -m "stopped: JS Variables"
 git push origin main
 git status
 
-# stopped in: What is JavaScript?
+# moment: 56:10
